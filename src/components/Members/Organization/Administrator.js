46cd9c58ce47cae34bff47f6d1bf9cada44b2140
박준희 <jhpark0256@naver.com>
2025-06-30 23:47:23 +0900

    동그리 얜가

--- src/components/Members/Organization/Administrator.js
+++ src/components/Members/Organization/Administrator.js
@@ -166,7 +166,7 @@ const GenText = styled.div`
   margin-top: 1.21rem;
   margin-bottom: 0.56rem;
 
-  width: 4.4375rem;
+  width: 10rem;
   height: 1.625rem;
   border-radius: 3.125rem;
   background: #9e1f15;
